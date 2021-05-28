042ebbbb1f5f8bae50be904faa343ec03f5ad150
Andrew Pierce <andrew.pierce@red-gate.com>
2021-05-28 03:47:58 -0700

    waaaat

--- sql/Versioned/V1.0.0__CustomersTable.sql
+++ sql/Versioned/V1.0.0__CustomersTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE customers (
     customer_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     name VARCHAR(25) NOT NULL,
-    address VARCHAR(50) NULL,
-    email VARCHAR(30) NULL 
+    address VARCHAR(50),
+    email VARCHAR(30)
 PRIMARY KEY(customer_id)
 );
